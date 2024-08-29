e76f542d3802877248e6590d49f060b435a7836b
Atiqur Rahman <atiqur.main@gmail.com>
2024-08-29 01:33:39 -0500

    setup font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>acm uta</h1>
+      <h1 className="font-extrabold">acm uta</h1>
     </main>
   );
 }
