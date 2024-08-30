e5b9087e9b52f852c15ccefdc840ca3e5ac18f3d
Atiqur Rahman <atiqur.main@gmail.com>
2024-08-30 17:27:49 -0500

    add about acm card

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { AboutAcmCard } from "@/components/AboutAcmCard";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <AboutAcmCard />
+    </>
+  );
+};
+
+export default page;
