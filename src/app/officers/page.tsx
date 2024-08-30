39901633dc03e04de18f26ca4c1a7ab2730b611f
Talha Tahmid <talhathmd@gmail.com>
2024-08-30 17:29:53 -0500

    Officers page

---
+++ src/app/officers/page.tsx
@@ -0,0 +1,10 @@
+export default function Officers() {
+    return (
+      <main className="flex min-h-screen flex-col items-start justify-between p-32">
+        <h1 className="font-bold text-5xl">meet the team</h1>
+        <p className="font-medium mt-10 text-2xl">5 Division One Goal.</p>
+
+        <h1 className="font-bold text-4xl">the board</h1>
+      </main>
+    );
+  }
