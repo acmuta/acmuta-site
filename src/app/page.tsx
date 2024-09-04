7d090171a48184b956d1fc0f5b4103c5add1308e
Atiqur Rahman <atiqur.main@gmail.com>
2024-09-04 02:41:42 -0500

    #2 add sponsors section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <div className="bg-white/10 rounded-[30px] md:rounded-[60px] px-8 py-8 md:px-24 md:py-12 max-w-lg md:max-w-2xl text-white border-2 border-white/40">
+    <main className="flex min-h-screen flex-col justify-between">
+      <div className="bg-white/10 rounded-[30px] md:rounded-[60px] px-8 py-8 md:px-24 md:py-12 max-w-lg md:max-w-2xl text-white border-2 border-white/40 mx-auto mt-24 md:ml-24">
         <div className="flex flex-col items-center mb-6">
           <h1 className="text-4xl md:text-5xl font-bold">
             association for computing machinery @ ut arlington
@@ -25,6 +25,12 @@ export default function Home() {
             </button>
           </Link>
         </div>
+      </div>
+
+      <div className="w-[85%] md:w-6/12 h-auto md:h-72 my-12 bg-white/10 rounded-[30px] md:rounded-tr-[0px] md:rounded-br-[0px] border-r-2 md:border-r-[0px] md: border-l-2 border-t-2 border-b-2 border-white/40 mx-auto md:mx-[0px] md:ml-auto p-12 flex flex-col items-center md:items-start justify-center space-y-4">
+        <h1 className="text-3xl md:text-4xl font-bold">
+          Sponsors and Partners
+        </h1>
       </div>
     </main>
   );
