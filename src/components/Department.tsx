29acc4dd976a24a0d904f8d2414aa3583b4c69d2
Talha Tahmid <talhathmd@gmail.com>
2024-09-06 15:25:58 -0500

    #3 Fixed text side

--- src/components/Department.tsx
+++ src/components/Department.tsx
@@ -33,7 +33,7 @@ const Department: React.FC<DepartmentProps> = ({ name, logoUrl, people }) => {
         <h2 className="text-4xl font-bold">{name}</h2>
       </div>
 
-      <div className="grid ml-20 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid sm:ml-0 lg:ml-20 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {people.map((person, index) => (
           <People key={index} {...person} />
         ))}
