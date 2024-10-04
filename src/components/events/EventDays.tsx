4eb477c10fecca2492308a3fa1fc878a33ec26f1
Atiqur Rahman <115911894+atiqurx@users.noreply.github.com>
2024-10-04 01:06:35 +0000

    backtrack debug

--- src/components/events/EventDays.tsx
+++ src/components/events/EventDays.tsx
@@ -1,37 +1,37 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
-import type { Event } from "@/lib/types.d.ts";
+// import { useEffect, useState } from "react";
+// import type { Event } from "@/lib/types.d.ts";
 
-type Props = {
-  event: Event;
-  day: number;
-  month: number;
-};
+// type Props = {
+//   event: Event;
+//   day: number;
+//   month: number;
+// };
 
-const EventComponent = ({ event, day, month }: Props) => {
-  const [localStartDate, setLocalStartDate] = useState<Date | null>(null);
+// const EventComponent = ({ event, day, month }: Props) => {
+//   const [localStartDate, setLocalStartDate] = useState<Date | null>(null);
 
-  useEffect(() => {
-    if (event.start) {
-      const localDate = new Date(event.start);
-      setLocalStartDate(localDate);
-    }
-  }, [event]);
+//   useEffect(() => {
+//     if (event.start) {
+//       const localDate = new Date(event.start);
+//       setLocalStartDate(localDate);
+//     }
+//   }, [event]);
 
-  if (
-    !localStartDate ||
-    localStartDate.getDate() !== day ||
-    localStartDate.getMonth() !== month
-  ) {
-    return null;
-  }
+//   if (
+//     !localStartDate ||
+//     localStartDate.getDate() !== day ||
+//     localStartDate.getMonth() !== month
+//   ) {
+//     return null;
+//   }
 
-  return (
-    <div className="mt-2 truncate rounded bg-gradient-to-r from-red-700 to-red-900 p-1 text-xs font-medium text-white">
-      {event.title}
-    </div>
-  );
-};
+//   return (
+//     <div className="mt-2 truncate rounded bg-gradient-to-r from-red-700 to-red-900 p-1 text-xs font-medium text-white">
+//       {event.title}
+//     </div>
+//   );
+// };
 
-export default EventComponent;
+// export default EventComponent;
