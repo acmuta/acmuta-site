b177433c12d9d4f272aa0a32e593f17f329f0337
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2025-02-03 21:18:44 -0600

    Fixed blurring and padding

--- src/app/acm-create/page.tsx
+++ src/app/acm-create/page.tsx
@@ -5,12 +5,12 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <div className="z-10 backdrop-blur-md bg-white/10 text-white py-12 px-4 my-8">
+      <div className="z-10 bg-white/10 text-white py-2 px-4 my-8">
         <div className="text-center mb-8">
           <h2 className="text-5xl font-bold mb-4">Projects Details</h2>
         </div>
         <RecentProject />
-        {/* <ContributionsTable /> */}
+        <ContributionsTable />
       </div>
     </div>
   );
